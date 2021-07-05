a86ef93a25a123618126ce3cdd6ffcae3e3ddab1
SnatcherAM <70380257+SnatcherAM@users.noreply.github.com>
2021-07-05 16:38:09 +0300

    Signup component

--- src/App.js
+++ src/App.js
@@ -1,34 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import Signup from './components/signup/signup.js';
 
 function App() {
   return (
     <div class="wrapper">
-        <div class="head">
-            <div class="head__row">
-                <div class="cityname">Kharkiv</div>
-                <div class="icon"></div>
-            </div>
-            <div class="head__row">
-                <div class="temp">+30</div>
-                <div class="icon-descr"></div>
-            </div>
-        </div>
-        <div class="bottom">
-            <div class="humidity">
-            Humidity
-                <div>70%</div>
-            </div>
-            <div class="windspeed">
-            Wind speed
-                <div>5 m/s</div>
-            </div>
-            <div class="pressure">
-            Pressure
-                <div>50</div>
-            </div>
-        </div>
-        <div class="date">02.07.2021</div>
+        <Signup />
     </div>
   );
 }
